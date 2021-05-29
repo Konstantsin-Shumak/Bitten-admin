9214e6fc57ab0cf2abbc8a6fa8e74a30ea447cf0
Шумак Константин <konstantsin.shumak@gmail.com>
2021-05-29 11:51:17 +0300

    addFirebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { FacebookButton } from "./Components/FacebookButton/FacebookButton";
 import "./App.css";
 
 export const App = () => {
   return (
-    <>
-    </>
+    <div className="wrapper">
+      <FacebookButton />
+    </div>
   );
 }
